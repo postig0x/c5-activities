3eae89851279a69c5f92f5a1e2c89e29079cbf97
postig0x <postigo.kura@gmail.com>
2024-08-19 18:15:43 +0000

    all 7 menu options working

--- 03_sys_info/system_info.sh
+++ 03_sys_info/system_info.sh
@@ -7,6 +7,15 @@
 # loop
 while true
 do
+  # display menu
+  echo "1. IP Addresses"
+  echo "2. Current User"
+  echo "3. CPU Information"
+  echo "4. Memory Information"
+  echo "5. Top 5 Memory Processes"
+  echo "6. Top 5 CPU Processes"
+  echo "7. Network Connectivity"
+
   # read 1 char as input with a prompt
   read -n 1 -p "Choose an option from the menu (type 'q' to quit): " input
   echo
@@ -16,29 +25,44 @@ do
       break
       ;;
     1)
-      echo "entered 1: $input"
+      public_ip=$(curl -s ifconfig.me)
+      private_ip=$(ip -4 addr show | grep inet | tail -n 1 | awk '{ print $2 }' | cut -d'/' -f1)
+      echo -e "\nIP ADDRESSES"
+      echo "PUBLIC IP: $public_ip"
+      echo -e "PRIVATE IP: $private_ip\n\n"
       ;;
     2)
-      echo "entered 2: $input"
+      echo -e "\nCURRENT USER"
+      echo -e "User is: $(whoami)\n\n"
       ;;
     3)
-      echo "entered 3: $input"
+      echo -e "\nCPU INFORMATION"
+      echo -e "The system has $(nproc) CPU\n\n"
       ;;
     4)
-      echo "entered 4: $input"
+      free_mem=$(free | sed -n '2p' | awk '{ print $4 }')
+      total_mem=$(free | sed -n '2p' | awk '{ print $2 }')
+      echo -e "\nMEMORY INFORMATION"
+      echo -e "There is $free_mem Mb unused memory of total $total_mem Mb\n\n"
       ;;
     5)
-      echo "entered 5: $input"
+      echo -e "\nTOP 5 MEMORY PROCESSES"
+      echo -e "$(ps aux | sort -k 4 -n -r | head -n 5 | awk '{print $4 "% from command: " $11}')\n\n"
       ;;
     6)
-      echo "entered 6: $input"
+      echo -e "\nTOP 5 CPU PROCESSES"
+      echo -e "$(ps aux | sort -k 3 -n -r | head -n 5 | awk '{print "USING " $3 "% CPU:"; for (i=11; i <= NF; i++) printf "%s", $i; printf "\n" }')\n\n"
       ;;
     7)
-      echo "entered 7: $input"
+      echo -e "\nNETWORK PROCESSES"
+      read -p "Please provide a website or IP address: " destination
+      connection_time=$(curl -o /dev/null -s -w "%{time_total}" $destination)
+      echo -e "It took $connection_time seconds to connect to $destination\n\n"
       ;;
     *)
       echo "input '$input' not recognized"
       ;;
   esac
+  sleep 1
 done
 
