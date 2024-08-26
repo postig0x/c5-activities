5162c750d30559ad9a6418a3d6584ebee812812b
postig0x <postigo.kura@gmail.com>
2024-08-26 18:13:50 -0400

    date script done

--- 06_date_py/date.py
+++ 06_date_py/date.py
@@ -16,28 +16,55 @@ try:
 except ValueError:
     print("Error: budget should be an integer or decimal.")
 
+# make key value pairs for menu item and price
+price_list = {}
+for i, itemObj in enumerate(food_menu.values()):
+    for item in itemObj.keys():
+        price_list[item] = itemObj[item]['price']
+
+# grab cheapest item price from menu
+price_limit = min(price_list.values())
+
 # 3. Print the restaurant menu
-# grab cheapest item price from menu loop
-price_limit = 0
-order = ''
 def show_menu():
+    global order
     print("Today's Menu")
     for i, (cat, blob) in enumerate(food_menu.items()):
         print(cat.title())
+        print()
         for j, (item, obj) in enumerate(blob.items()):
-            print(item.title() + ":", obj['price'])
-            if obj['price'] < price_limit or price_limit == 0:
-                price_limit = obj['price']
-    order = input("What would you like to order? ")
+            print(item.title() + ":", f"{obj['price']:.2f}")  
+    print()
+    order = input("What would you like to order? ").lower()
 
-# get cheapest item value
 # 4. User inputs their food/drink item choices
+items_ordered = 0 # count how many items the user orders
+# when the budget is less than the price of the cheapest item, the date will end
 while budget > price_limit:
     show_menu()
-    print(food_menu.values())
-
-# 5. Tell the user how much money they have left after each order
+    if order == 'leave':
+        print("You have left the restaurant.")
+        break
+    try:
+        budget -= price_list[order]
+        items_ordered += 1
+        # 5. Tell the user how much money they have left after each order
+        print(f"You have ${budget:.2f} left after your order.")
+        print()
+    except KeyError:
+        print("Error: invalid item")
+        print()
 
 # 6. At the end, user must agree to pay the bill and then their final bill is shown to them
+if order != 'leave':
+    terms = input("Do you agree to pay the bill? (yes/no) ")
+    if terms.lower() == 'yes':
+        print(f"You paid ${start_budget - budget:.2f} for your date.")
+    else:
+        print("You did not agree to pay the bill.")
 
 # 7. Challenge: based on all user inputs, script should decide if they will get a 2nd date
+if (items_ordered > 4):
+    print("You will get a second date.")
+else:
+    print("You will not get a second date.")
