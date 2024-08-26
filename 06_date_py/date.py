90363614b97f45e6c2ae68c0d5cde64dc892d627
postig0x <postigo.kura@gmail.com>
2024-08-26 21:46:37 +0000

    working on date script

--- 06_date_py/date.py
+++ 06_date_py/date.py
@@ -3,18 +3,38 @@
 
 # DATE SIMULATOR
 
+# import food menu
+from menu import food_menu
+
 # 1. User inputs who is on the date with them
 date = input("Who is your date? ")
 
 # 2. User inputs budget for the date
 try:
-    budget = float(input("What is your budget? "))
+    start_budget = float(input("What is your budget? "))
+    budget = start_budget
 except ValueError:
     print("Error: budget should be an integer or decimal.")
 
 # 3. Print the restaurant menu
+# grab cheapest item price from menu loop
+price_limit = 0
+order = ''
+def show_menu():
+    print("Today's Menu")
+    for i, (cat, blob) in enumerate(food_menu.items()):
+        print(cat.title())
+        for j, (item, obj) in enumerate(blob.items()):
+            print(item.title() + ":", obj['price'])
+            if obj['price'] < price_limit or price_limit == 0:
+                price_limit = obj['price']
+    order = input("What would you like to order? ")
 
+# get cheapest item value
 # 4. User inputs their food/drink item choices
+while budget > price_limit:
+    show_menu()
+    print(food_menu.values())
 
 # 5. Tell the user how much money they have left after each order
 
--- 06_date_py/menu.py
+++ 06_date_py/menu.py
@@ -14,7 +14,7 @@
 # create appetizers
 
 food_menu = {
-    "appetizer": {
+    "appetizers": {
         "samosa": {
             "price": 3.00
         },
@@ -28,50 +28,62 @@ food_menu = {
             "price": 7.50
         }
     },
-    "main course": {
+    "main courses": {
         "lasagna": {
-            "description": "Type of layered pasta dish"
+            "description": "Type of layered pasta dish",
+            "price": 18.00
         },
         "pizza": {
-            "description": "A cheesy pie"
+            "description": "A cheesy pie",
+            "price": 16.00,
         },
         "burger": {
-            "description": "It's a burger"
+            "description": "It's a burger",
+            "price": 10.00,
         },
         "paella": {
-            "description": "Medley of rice, seafood, sausage, vegetables"
+            "description": "Medley of rice, seafood, sausage, vegetables",
+            "price": 20.00
         }
     },
     "desserts": {
         "milkshake": {
-            "calories": 400
+            "calories": 400,
+            "price": 4.00,
         },
         "sundae": {
-            "calories": 600
+            "calories": 600,
+            "price": 6.00,
         },
-        "lava_cake": {
-            "calories": 800
+        "lava cake": {
+            "calories": 800,
+            "price": 8.00,
         },
         "sorbet": {
-            "calories": 200
+            "calories": 200,
+            "price": 6.00
         }
     },
     "beverages": {
         "mango lassi": {
             "is_vegan": False,
-            "ingredients": ['mango', 'yogurt', 'water']
+            "ingredients": ['mango', 'yogurt', 'water'],
+            "price": 6.00,
         },
         "thai iced tea": {
             "is_vegan": False,
-            "ingredients": ['milk', 'black tea', 'sugar']
+            "ingredients": ['milk', 'black tea', 'sugar'],
+            "price": 3.50,
         },
         "sugarcane juice": {
             "is_vegan": True,
-            "ingredients": ['sugarcane', 'ice']
+            "ingredients": ['sugarcane', 'ice'],
+            "price": 4.50,
         },
         "water": {
             "is_vegan": True,
-            "ingredients": ['hydrogen^2', 'oxygen']
+            "ingredients": ['hydrogen^2', 'oxygen'],
+            "price": 3.00,
         }
     }
 }
