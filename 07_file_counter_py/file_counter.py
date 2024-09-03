37930685a6754a9e664299d13fbd7bbac568bbe6
postig0x <postigo.kura@gmail.com>
2024-09-03 22:28:05 +0000

    07_file_counter_py added

---
+++ 07_file_counter_py/file_counter.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+# 09/03/2024
+# File Counter
+# Count the number of files and directories in a specified directory (take dir as input)
+# Bonus: modify script to count the number of files and directories separately
+
+import os
+import sys
+
+# initialize counter variables
+files = 0
+dirs = 0
+
+# ask for directory path
+dir_path = input("What directory do you want to count? ")
+
+# check if path exists
+if not os.path.exists(dir_path):
+    print(f"{dir_path} does not exist")
+    sys.exit(1)
+
+# check if path is a directory
+if not os.is_dir(dir_path):
+    print(f"{dir_path} is not a directory")
+    sys.exit(1)
+
+for item in os.listdir(dir_path):
+    item_path = os.path.join(dir_path, item)
+    if os.path.is_file(item_path):
+        files += 1
+    elif os.path.is_dir(item_path):
+        dirs += 1
+
+
+print(f"Directory {dir_path} contains:")
+print(f"{files} files")
+print(f"{dirs} directories")
